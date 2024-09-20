8d96286e0915488cca9cb3fbe2b531e9cbfbb435
bulka <jej07267@gmail.com>
2024-09-20 22:43:47 +0300

    0.120

---
+++ src/x86-64/govnosconst.py
@@ -0,0 +1,9 @@
+# !@#$%^&*()[]{};'\:"|,./<>?=_+`~
+GVS_UC_KEY_SPECSYMS = [
+  33, 64, 35, 36, 37, 94, 38, 42, 40, 41, 91, 93, 123, 125, 59, 39, 92, 58, 34, 124, 44, 46, 47,
+  60, 62, 63, 61, 95, 43, 96, 126
+];
+GVS_UC_KEY_LETTERS = list(range(65, 91)) + list(range(97, 123));
+GVS_UC_KEY_LETTERS_CAP = list(range(65, 91));
+GVS_UC_KEY_LETTERS_SML = list(range(97, 123));
+GVS_UC_KEY_DIGITS = list(range(48, 58));
